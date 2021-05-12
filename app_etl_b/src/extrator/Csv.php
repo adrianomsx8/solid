ddd18938a030884eb2a22babe266a299708c68f4
Adriano Souza <adriano.msx8@gmail.com>
2021-05-12 15:17:34 -0300

    Primeiro Commit

---
+++ app_etl_b/src/extrator/Csv.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace src\extrator;
+
+class Csv extends Arquivo{
+
+    public function lerArquivo(string $caminho): array{
+      
+        $handle = fopen($caminho, 'r');
+
+        while(($linha = fgetcsv($handle, 10000, ';')) !== false){
+           //print_r($linha);
+           $this->setDados($linha[0], $linha[1], $linha[2]);
+        }
+
+        fclose($handle);
+
+        return  $this->getDados();
+    }
+    
+}
